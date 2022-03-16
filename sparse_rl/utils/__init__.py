84b42b10280e356d3e173e52a6eaa6d2e0409a65
jc-bao <jc-bao@outlook.com>
2022-03-16 20:00:01 +0800

    add wandb sweep support

--- sparse_rl/utils/__init__.py
+++ sparse_rl/utils/__init__.py
@@ -1,4 +1,22 @@
 from sparse_rl.utils.vec_env import SubprocVecEnv
 from sparse_rl.utils.normalizer import Normalizer, ArrayNormalizer
+import gym
 
 __all__ = ['SubprocVecEnv', 'Normalizer', 'ArrayNormalizer']
+
+def get_env_params(env_name, env_kwargs):
+    env = gym.make(env_name, **env_kwargs)
+    obs = env.reset()
+    params = {
+        'gripper': obs['gripper_arr'].shape[-1],
+        'goal': obs['desired_goal_arr'].shape[-1],
+        'action': env.action_space.shape[0],
+        'action_max': env.action_space.high[0],
+        'object': obs['object_arr'].shape[-1],
+        'n_objects': obs['object_arr'].shape[0],
+        'max_timesteps': env._max_episode_steps, 
+        'compute_reward': env.compute_reward
+    }
+    env.close()
+    return params
+
