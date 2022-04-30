0833a19be89af007bd2496acfbd0e6022a607ef7
jc-bao <jc-bao@outlook.com>
2022-04-30 17:14:03 +0800

    fix panda import erroes

--- experiment/train.py
+++ experiment/train.py
@@ -7,14 +7,13 @@ from omegaconf import OmegaConf
 import gym
 from sparse_rl.agent import ddpg_agent
 from sparse_rl.utils import SubprocVecEnv, get_env_params
+import panda_gym
 
 @hydra.main(config_name='main', config_path='config')
 def launch(cfg = None):
     # 1. make env
     env_params = get_env_params(cfg.env_name, cfg.env_kwargs)
     def make_env():
-        from importlib.machinery import SourceFileLoader
-        modulevar = SourceFileLoader('FrankaCube', '/home/reed/rl/srl/envs/franka_cube.py').load_module()
         return gym.make(cfg.env_name, **cfg.env_kwargs)
     env = SubprocVecEnv([make_env for i in range(cfg.num_workers)])
 
